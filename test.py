88fb53a3dff7a27ecf5753e2b5082b33495d3c0f
Namprire <napham@outlook.de>
2025-01-17 13:38:36 +0900

    Update comments in test.py to enhance clarity on model loading and performance recommendations

--- test.py
+++ test.py
@@ -3,18 +3,24 @@ from qwen_vl_utils import process_vision_info
 import torch
 
 # default: Load the model on the available device(s)
+
 '''
 model = Qwen2VLForConditionalGeneration.from_pretrained(
     "Qwen/Qwen2-VL-2B-Instruct", torch_dtype="auto", device_map="auto"
-) '''
+) 
+
+'''
 
 # We recommend enabling flash_attention_2 for better acceleration and memory saving, especially in multi-image and video scenarios.
+
+
 model = Qwen2VLForConditionalGeneration.from_pretrained(
     "Qwen/Qwen2-VL-2B-Instruct",
     torch_dtype=torch.bfloat16,
     attn_implementation="flash_attention_2",
     device_map="auto",
 )
+
 
 # default processer
 processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct")
